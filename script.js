e7e3525fecec5e0ba7ef3c738df5af8fd0ada230
BenasPalivonas <69222998+BenasPalivonas@users.noreply.github.com>
2021-02-24 17:23:44 +0200

    commiting

--- script.js
+++ script.js
@@ -4,6 +4,49 @@ var text = document.getElementById("text");
 var command = "";
 var scoreText = document.getElementById("score");
 var score = 0;
+var gamestarted = false;
+var button = document.getElementById("button");
+var proTip = document.getElementById("proTip");
+const openModalButton = document.getElementById("open-button");
+var name = document.getElementById("name");
+const submitScore = (name, score) => {
+    if (name) {
+        fetch('https://hidden-mesa-40035.herokuapp.com/addToLeaderBoard', {
+            method: 'post',
+            headers: { 'Content-Type': "application/json" },
+            body: JSON.stringify({
+                name: name,
+                score: score
+            })
+        }).then(response => response.json()).then(data => {
+            console.log(data);
+        }
+        )
+    }
+}
+var startGame = () => {
+    leaderBoard.style.display = "none";
+    openModalButton.style.display = "none";
+    name.style.visibility = "hidden";
+    console.log("clicking");
+    score = 0;
+    gamestarted = true;
+    block.style.visibility = "visible";
+    block.classList.add("animateBlock")
+    button.style.display = "none";
+    proTip.style.display = "none";
+    var scoreInterval = setInterval(() => {
+        console.log("adding");
+        if (gamestarted == true) {
+            score++;
+        }
+        if (gamestarted == false) {
+            clearInterval(scoreInterval);
+        }
+        return 0;
+    }, 6000);
+}
+button.onclick = startGame;
 //speech recognition setup
 var message = document.querySelector('#message');
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
@@ -22,7 +65,7 @@ recognition.onresult = function (event) {
     console.log('Voice Input: ' + command + '.');
     text.textContent = 'Voice Input : ' + command + '.';
     if (!character.classList.contains("animate")) {
-        if (command.toLowerCase() == "monkey jump") {
+        if (command.toLowerCase().includes("monkey jump")) {
             character.classList.add("animate");
         }
         else if (command.toLowerCase().includes("monkey joe") == true) {
@@ -33,7 +76,7 @@ recognition.onresult = function (event) {
 
         setTimeout(() => {
             character.classList.remove("animate");
-        }, 1500);
+        }, 2500);
     }
 };
 recognition.onspeechend = function () {
@@ -43,13 +86,15 @@ recognition.onspeechend = function () {
 recognition.onerror = function (event) {
     console.log('Error occurred in recognition: ' + event.error);
 }
-recognition.start();
+
+
+
 var checkDead = setInterval(() => {
     var topPosition = parseFloat(window.getComputedStyle(character).getPropertyValue("top"));
     var leftPoistion = parseFloat(window.getComputedStyle(block).getPropertyValue("left"));
     var bottomPosition = parseFloat(window.getComputedStyle(character).getPropertyValue("bottom"));
 
-    if (bottomPosition == -150) {
+    if (bottomPosition == -290) {
         try {
             recognition.start();
         }
@@ -57,19 +102,91 @@ var checkDead = setInterval(() => {
 
         }
     }
-    if (leftPoistion < 20 && leftPoistion > 0) {
-        score++;
-    }
-    if (leftPoistion < 20 && leftPoistion > 0 && topPosition >= 130) {
-        //endGame();
-        console.log("hit");
+    if (leftPoistion < 300 && leftPoistion > 0 && topPosition >= 200) {
+
+        endGame();
     }
     scoreText.textContent = "Score: " + score;
-}, 50)
-var endGame = () => {
 
-    block.style.animation = "none";
-    block.style.display = "none";
-    character.style.display = "none";
-    alert("game over");
+}, 50)
+
+var endGame = () => {
+    addLeaders();
+    console.log("called")
+    submitScore(name.value.toLowerCase(), score);
+    gamestarted = false;
+    block.style.visibility = "hidden";
+    button.style.display = "block";
+    openModalButton.style.display = "block";
+    block.classList.remove("animateBlock");
+    proTip.style.display = "block";
+    name.style.visibility = "visible";
+    leaderBoard.style.display = "block";
+
 }
+
+
+//skins
+var test = () => {
+    console.log("test");
+}
+
+var ul = document.getElementById("skins");
+import skins from "./skinList.js";
+for (let i = 0; i < skins.length; i++) {
+    var li = document.createElement("li");
+    li.style.backgroundImage = "url" + "(" + skins[i].url + ")";
+    li.onclick = () => {
+        console.log(skins[i].url);
+        character.style.backgroundImage = "url" + "(" + skins[i].url + ")";
+    };
+    ul.appendChild(li);
+    //li.setAttribute('id', skins[i].Name);
+    //li.appendChild(document.createTextNode(skins[i].Name));
+}
+
+//overlay
+
+const closeModalButton = document.getElementById("close-button");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+openModalButton.onclick = () => {
+    modal.classList.add("active");
+    overlay.classList.add("active");
+}
+const removeOverLay = () => {
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+}
+closeModalButton.onclick = () => {
+    removeOverLay();
+}
+
+overlay.onclick = () => {
+    removeOverLay();
+}
+
+//LeaderBoard
+var leaderBoard = document.getElementById("leaderBoard");
+console.log(leaderBoard);
+const addLeaders = () => {
+    leaderBoard.replaceChildren();
+    var p = document.createElement("p");
+    p.setAttribute('id', "leaderText");
+    p.appendChild(document.createTextNode("Leader Board"));
+    leaderBoard.appendChild(p);
+    fetch('https://hidden-mesa-40035.herokuapp.com/top3').then(response => response.json())
+        .then((leaders) => {
+            leaders.map(leader => {
+                var li = document.createElement("li");
+                //li.setAttribute('id', leader.Name);
+                li.appendChild(document.createTextNode(leader.name + " : " + leader.score));
+                leaderBoard.appendChild(li);
+
+            });
+
+
+        })
+}
+
+addLeaders();
---
+++ skinList.js
@@ -0,0 +1,75 @@
+
+
+var skins = [
+    {
+        url: "realmonke.png"
+    },
+    {
+        url: "AMONGUS.png"
+    },
+    {
+        url: "beachMonkey.png"
+    },
+    {
+        url: "gangsta.png"
+    },
+    {
+        url: "purple.png"
+    },
+    {
+        url: "runescape.png"
+    },
+    {
+        url: "cuteshrek.png"
+    },
+    {
+        url: "drtmonkey.png"
+    },
+    {
+        url: "homersipmson.png"
+    }
+    , {
+        url: "minionplayer.png"
+    },
+    {
+        url: "mario.png"
+    },
+    {
+        url: "dbz.png"
+    },
+    {
+        url: "southpark.png"
+    },
+    {
+        url: "hitman.png"
+    },
+    {
+        url: "censored.png"
+    },
+    {
+        url: "basketball.png"
+    },
+    {
+        url: "johny.png"
+    },
+    {
+        url: "FEET.png"
+    },
+    {
+        url: "joker.png"
+    }, {
+        url: "noshirt.png"
+    },
+    {
+        url: "batman.png"
+    }, {
+        url: "hellokitty.png"
+    },
+    {
+        url: "drake.png"
+    },
+    {
+        url: "lilpeep.png"
+    }
+]
+export default skins;
